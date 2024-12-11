7889fb99cc4750afdc5ae6eddeb2679b6b9dcc72
Mika <mika.terhokoski@pietiko.fi>
2024-12-11 13:51:02 +0200

    Add 2023 day 19 part 2

--- 2023/day19/part2.py
+++ 2023/day19/part2.py
@@ -1,9 +1,11 @@
 
 from cProfile import Profile
 from pstats import SortKey, Stats
+import copy
+import itertools
 
 input_file = "example.txt"
-#input_file = "input.txt"
+input_file = "input.txt"
 
 with open(input_file, "r") as file:
     data = file.readlines()
@@ -13,6 +15,11 @@ data = [l.strip() for l in data]
 empty_line = data.index("")
 workflow_data = data[:empty_line]
 part_data = data[empty_line + 1:]
+
+def change_tuple(t:tuple, i:int, value):
+    l = list(t)
+    l[i] = value
+    return tuple(l)
 
 parts = []
 for line in part_data:
@@ -44,36 +51,52 @@ for line in workflow_data:
     default = rule_data[-1]
 
     workflows[name] = (rules, default)
-#print(workflows)
 
+#for k,v in workflows.items():
+#    print(k,v)
 
-def process_part(part:list[int], workflow:str):
+ranges = set()
+
+def process_part(part:list[int], workflow:str, limits:tuple[tuple[int,int]], i:int=0):
     if workflow == "A":
-        return "A"
+        ranges.add(tuple(limits))
+        return
+
     if workflow == "R":
-        return "R"
+        return
 
     rules, default = workflows[workflow]
 
     for rule in rules:
-        if rule[1] == "<":
-            if part[rule[0]] < rule[2]:
-                return process_part(part, rule[3])
+        cat, op, value, target = rule
+        cat_l = limits[cat]
+        if op == "<":
+            next_range = (cat_l[0], min(value - 1, cat_l[1]))
+            new_range = (value, cat_l[1])
+
         else:
-            if part[rule[0]] > rule[2]:
-                return process_part(part, rule[3])
+            next_range = (max(value + 1, cat_l[0]), cat_l[1])
+            new_range = (cat_l[0], value)
 
-    return process_part(part, default)
 
+        next_limit = change_tuple(limits, cat, next_range)
+        process_part(part, target, next_limit, i+1)
+
+        limits = change_tuple(limits, cat, new_range)
+
+
+    process_part(part, default, limits, i+1)
+
+#Find all possible ranges for each part
+limits = [(1,4000),(1,4000),(1,4000),(1,4000)]
+for part in parts:
+     process_part(part, "in", limits)
 
 total = 0
-for part in parts:
-    target = process_part(part, "in")
-    if target == "A":
-        total += sum(part)
+for r in ranges:
+    range_total = 1
+    for i in range(4):
+        range_total *= r[i][1] - r[i][0] + 1
+    total += range_total
 
 print(total)
-
-
-
-
